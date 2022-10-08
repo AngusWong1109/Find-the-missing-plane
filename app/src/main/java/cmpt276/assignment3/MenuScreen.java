3ae8820f16d093d43c5df34755769cf534d4fe50
ccw21 <ccw21@sfu.ca>
2022-10-08 10:55:51 -0700

    Working on Menu screen
    Setting up buttons on menu screen, linked each button to related screen.

--- app/src/main/java/cmpt276/assignment3/MenuScreen.java
+++ app/src/main/java/cmpt276/assignment3/MenuScreen.java
@@ -2,7 +2,10 @@ package cmpt276.assignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuScreen extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MenuScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_screen);
+        Button newGame = findViewById(R.id.btnNewGame);
+        newGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuScreen.this, GameScreen.class);
+                startActivity(i);
+            }
+        });
+        Button options = findViewById(R.id.btnOptions);
+        options.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuScreen.this, OptionScreen.class);
+                startActivity(i);
+            }
+        });
+        Button help = findViewById(R.id.btnHelp);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuScreen.this, HelpScreen.class);
+                startActivity(i);
+            }
+        });
     }
 }
